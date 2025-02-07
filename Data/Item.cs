eb2a20a3ee84fd235d2ed2fc0317a50576276cc5
KJane0120 <rlawoals5755@gmail.com>
2025-02-07 16:36:46 +0900

    [Modify]변수명 수정
    변수명 간결하게 수정했습니다.

--- Data/Item.cs
+++ Data/Item.cs
@@ -7,36 +7,57 @@ using System.Threading.Tasks;
 
 namespace Nightmare.Data
 {
-    public class Item 
+    public class Item
     {
-        string Name { get; set; }
-        ItemType Type { get; set; }
-        int Attack { get; set; }
-        int Defense { get; set; }
-        int Health { get; set; }
-        int Avoidance { get; set; }
-        int Critical { get; set; }
-        string Description { get; set; }
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public ItemType Type { get; set; }
+        public int Atk { get; set; }
+        public int Def { get; set; }
+        public int Hp { get; set; }
+        public int Avd { get; set; }
+        public int Crt { get; set; }
+        public string? Desc { get; set; }
+        public int Cost { get; set; }
+        public bool IsPurchase { get; set; }
+        public bool IsEquip { get; set; }
 
         public enum ItemType
         {
-            //None,
+            None,
             [Description("무기")]
             Weapon,
             [Description("방어구")]
             Armor,
         }
+
+        public string GetTypeString()
+        {
+            string str = (Type == ItemType.Weapon) ? $"공격력 +{Atk}" : $"방어력 +{Def}";
+            return str;
+        }
+
+        public string GetPriceString()
+        {
+            string str = IsPurchase ? "구매 완료" : $"{Cost}";
+            return str;
+        }
+        public string ShowShopItem()
+        {
+            string str = $"{Name} | {GetTypeString()} | {Desc} | {GetPriceString()}";
+            return str;
+        }
     }
     public class ShopItem : Item
     {
-
+        
     }
 
     //public interface IEquip
     //{
     //    void Equip();
     //}
-    
+
     //public interface IUnEquip
     //{
     //    void UnEquip();
