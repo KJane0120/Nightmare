518534053c7e9e30cf0e332c64f4dc6c5e3d7010
KJane0120 <rlawoals5755@gmail.com>
2025-02-07 19:39:42 +0900

    [feat] 에러메시지 기능 추가
    보유골드와 선택아이템가격을 비교 후 그에 따른 메시지 출력
    구매 여부에 따른 메시지 출력
    로직을 생성했습니다.

--- Scripts/Action/Action_Buy.cs
+++ Scripts/Action/Action_Buy.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
+using Nightmare.Data;
 using static Nightmare.GameManager;
 
 namespace Nightmare
@@ -24,8 +25,53 @@ namespace Nightmare
                 };
             }
 
+            public void Buy(Item item)//아이템 구매 
+            {
+                ////골드가 충분할 때 
+                //if (player.Gold >= item.Cost)
+                //{
+                //    player.Gold -= item.Cost;
+                //    item.IsPurchase = true;
+                //    Action_Inventory.Add(item);
+                //    DisPlay();
+
+                //}
+                ////골드가 부족할 때 
+                //else
+                //{
+                //    //골드가 부족하다는 메시지 출력
+
+                //}
+            }
+            private void PrintErrorMsg(int number)
+            {
+                Item selectItem = DataManager.Instance.ShopItems[number];
+
+                if(player.gold >= selectItem.Cost)
+                {
+                    Console.WriteLine("구매가 완료되었습니다.");
+                    selectItem.IsPurchase = true;
+                }
+                else
+                {
+                    Console.WriteLine("골드가 부족합니다 !! ");
+                }
+                
+                //아이템이 구매한 적이 있다면 
+                if (selectItem.IsPurchase)
+                {
+                    Console.WriteLine("이미 보유한 아이템입니다.");
+                }
+                else
+                {
+                    Buy(selectItem);
+                    selectItem.IsPurchase = true;
+                }
+                
+            }
             protected override void DisPlay()
             {
+                OnInputInvalidActionNumber = PrintErrorMsg;
                 Console.Clear();
                 Console.WriteLine();
                 Console.WriteLine("상점 - 아이템 구매");
@@ -42,8 +88,17 @@ namespace Nightmare
                 }
 
                 Console.WriteLine();
+                //if(needGold)
+                //{
+                //    Console.WriteLine("골드가 부족합니다.");
+                //}
+                //else if(hasItem)
+                //{
+                //    Console.WriteLine("이미 보유한 아이템입니다.");
+                //}
+
                 
-                
+
 
 
             }
