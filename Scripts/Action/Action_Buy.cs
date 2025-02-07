1314df57cd243466190ee404e5994061ccf93845
KJane0120 <rlawoals5755@gmail.com>
2025-02-07 10:56:00 +0900

    [feat] 아이템 구매 액션 생성
    상점->아이템 구매를 위해 Buy 클래스를 생성했습니다

---
+++ Scripts/Action/Action_Buy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nightmare.Scripts.Action
+{
+    internal class Action_Buy
+    {
+    }
+}
