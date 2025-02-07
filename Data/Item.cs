f3296647ecf2d8a2285803bd165bda146cffe95b
KJane0120 <rlawoals5755@gmail.com>
2025-02-07 19:35:51 +0900

    [Modify]bool 기본값 할당
    기본값 할당함으로써 구매여부, 구매가능여부를 판별하여 메시지 출력을 가능케 함
    +주석으로 코드 설명 추가
    +필요없는 부분 삭제

--- Data/Item.cs
+++ Data/Item.cs
@@ -20,8 +20,8 @@ namespace Nightmare.Data
         public string? Desc { get; set; }
 
         public int Cost { get; set; }
-        public bool IsPurchase { get; set; }
-        public bool IsEquip { get; set; }
+        public bool IsPurchase { get; set; } = false;
+        public bool IsEquip { get; set; } = false;
 
         public enum ItemType
         {
@@ -31,47 +31,23 @@ namespace Nightmare.Data
             [Description("방어구")]
             Armor,
         }
-
+        //무기인지 방어구인지에 따라 공격력이나 방어력을 출력
         public string GetTypeString()
         {
             string str = (Type == ItemType.Weapon) ? $"공격력 +{Atk}" : $"방어력 +{Def}";
             return str;
         }
-
+        //구매 여부에 따른 출력
         public string GetPriceString()
         {
             string str = IsPurchase ? "구매 완료" : $"{Cost}";
             return str;
         }
+        //상점아이템 정보 출력 양식
         public string ShowShopItem()
         {
             string str = $"{Name} | {GetTypeString()} | {Desc} | {GetPriceString()}";
             return str;
         }
     }
-    public class ShopItem : Item
-    {
-        
-    }
-
-    //public interface IEquip
-    //{
-    //    void Equip();
-    //}
-
-    //public interface IUnEquip
-    //{
-    //    void UnEquip();
-    //}
-
-    //public interface IDrop
-    //{
-    //    void Drop();
-    //}
-
-    //public interface IPickUp
-    //{
-    //    void PickUp();
-    //}
-
 }
