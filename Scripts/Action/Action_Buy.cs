960b79a46e9c8a2836837137acbf907dc05943b0
KJane0120 <rlawoals5755@gmail.com>
2025-02-07 11:45:29 +0900

    [feat] 아이템 구매 클래스 구현
    아이템 구매 클래스 초기 구현

--- Scripts/Action/Action_Buy.cs
+++ Scripts/Action/Action_Buy.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Nightmare.GameManager;
 
-namespace Nightmare.Scripts.Action
+namespace Nightmare
 {
-    internal class Action_Buy
+    public partial class GameManager
     {
+        public class Action_Buy : ActionBase
+        {
+            public Action_Buy(int number) : base(number) { }
+
+            public override ActionType Type => ActionType.Buy;
+
+            protected override Dictionary<int, ActionBase> CreateNextActionDic()
+            {
+                return new Dictionary<int, ActionBase>()
+                {
+                    { 0,  new Action_Return(0) },
+                };
+            }
+
+            protected override void DisPlay()
+            {
+
+            }
+        }
     }
 }
