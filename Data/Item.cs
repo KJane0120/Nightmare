b9f299025ae2bd80dd413da258fe4370134e7e32
KJane0120 <rlawoals5755@gmail.com>
2025-02-07 11:55:50 +0900

    [feat]Item 클래스 생성
    아이템 클래스 생성했습니다.
    아이템 타입도 enum으로 구현했습니다
    //추후 ShopItem 리스트 추가 예정
    인터페이스 장착, 해제, 드랍, 줍기 기능 주석으로 달아놨으니 확인해주세요

---
+++ Data/Item.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nightmare.Data
+{
+    public class Item 
+    {
+        string Name { get; set; }
+        ItemType Type { get; set; }
+        int Attack { get; set; }
+        int Defense { get; set; }
+        int Health { get; set; }
+        int Avoidance { get; set; }
+        int Critical { get; set; }
+        string Description { get; set; }
+
+        public enum ItemType
+        {
+            //None,
+            [Description("무기")]
+            Weapon,
+            [Description("방어구")]
+            Armor,
+        }
+    }
+    public class ShopItem : Item
+    {
+
+    }
+
+    //public interface IEquip
+    //{
+    //    void Equip();
+    //}
+    
+    //public interface IUnEquip
+    //{
+    //    void UnEquip();
+    //}
+
+    //public interface IDrop
+    //{
+    //    void Drop();
+    //}
+
+    //public interface IPickUp
+    //{
+    //    void PickUp();
+    //}
+
+}
