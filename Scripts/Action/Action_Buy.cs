ea14ce734f760eeecef45f7af96a78e0df093ce8
KJane0120 <rlawoals5755@gmail.com>
2025-02-07 20:23:21 +0900

    [Feat]아이템 구매 후 에러메시지 출력 기능
    구매여부, 구매가능여부에 따른 에러메시지 출력 로직 기능 추가

--- Scripts/Action/Action_Buy.cs
+++ Scripts/Action/Action_Buy.cs
@@ -13,6 +13,7 @@ namespace Nightmare
     {
         public class Action_Buy : ActionBase
         {
+
             public Action_Buy(int number) : base(number) { }
 
             public override ActionType Type => ActionType.Buy;
@@ -25,49 +26,31 @@ namespace Nightmare
                 };
             }
 
-            public void Buy(Item item)//아이템 구매 
-            {
-                ////골드가 충분할 때 
-                //if (player.Gold >= item.Cost)
-                //{
-                //    player.Gold -= item.Cost;
-                //    item.IsPurchase = true;
-                //    Action_Inventory.Add(item);
-                //    DisPlay();
-
-                //}
-                ////골드가 부족할 때 
-                //else
-                //{
-                //    //골드가 부족하다는 메시지 출력
-
-                //}
-            }
             private void PrintErrorMsg(int number)
             {
                 Item selectItem = DataManager.Instance.ShopItems[number];
 
-                if(player.gold >= selectItem.Cost)
+                //아이템이 구매한 적이 있다면 
+                if (selectItem.IsPurchase == false)
                 {
-                    Console.WriteLine("구매가 완료되었습니다.");
-                    selectItem.IsPurchase = true;
+                    if (GameManager.Instance.Player.Gold.PlayerGold >= selectItem.Cost)
+                    {
+                        Console.WriteLine("구매가 완료되었습니다.");
+                        selectItem.IsPurchase = true;
+                        GameManager.Instance.Player.Gold.PlayerGold -= selectItem.Cost;
+                        DataManager.Instance.HaveItems.Add(selectItem.Id, selectItem);
+                        DisPlay();
+                    }
+                    else
+                    {
+                        Console.WriteLine("골드가 부족합니다 !! ");
+                    }
                 }
                 else
-                {
-                    Console.WriteLine("골드가 부족합니다 !! ");
-                }
-                
-                //아이템이 구매한 적이 있다면 
-                if (selectItem.IsPurchase)
                 {
                     Console.WriteLine("이미 보유한 아이템입니다.");
                 }
-                else
-                {
-                    Buy(selectItem);
-                    selectItem.IsPurchase = true;
-                }
-                
+
             }
             protected override void DisPlay()
             {
@@ -78,7 +61,7 @@ namespace Nightmare
                 Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.");
                 Console.WriteLine();
                 Console.WriteLine("[보유 골드]");
-                //Console.WriteLine($"{player.Gold} G");
+                Console.WriteLine($"{GameManager.Instance.Player.Gold.PlayerGold} G");
 
                 Console.WriteLine();
                 Console.WriteLine("[아이템 목록]");
@@ -88,18 +71,7 @@ namespace Nightmare
                 }
 
                 Console.WriteLine();
-                //if(needGold)
-                //{
-                //    Console.WriteLine("골드가 부족합니다.");
-                //}
-                //else if(hasItem)
-                //{
-                //    Console.WriteLine("이미 보유한 아이템입니다.");
-                //}
-
-                
-
-
+            
 
             }
         }
