75315482cfa0d884bc1c5fc5f7afa280845bea17
KJane0120 <rlawoals5755@gmail.com>
2025-02-12 00:35:49 +0900

    [Feat]포션목록 구현
    포션 목록 구현했습니다.

--- Scripts/Action/Action_Recovery.cs
+++ Scripts/Action/Action_Recovery.cs
@@ -24,23 +24,30 @@ namespace Nightmare
                 };
             }
 
-            //private void PrintErrorMsg(int number)
-            //{
-            //    Item selectItem = DataManager.Instance.ItemDatas[number];
-
-            //}
+            private void PrintErrorMsg(int number)
+            {
+                var selectPortion = DataManager.Instance.PortionDatas[number];
+                selectPortion.OnUsePortionEvent = DisPlay;
+                selectPortion.UsePortion();
+            }
             protected override void DisPlay()
             {
                 Console.Clear();
-              //  OnInputInvalidActionNumber = PrintErrorMsg;
+                OnInputInvalidActionNumber = PrintErrorMsg;
                 Console.WriteLine("회복");
                 Console.WriteLine("포션을 사용하여 HP나 MP를 회복할 수 있습니다.");
 
                 Console.WriteLine("[포션 목록]");
                 Console.WriteLine();
-                Console.WriteLine($"앨리스의 쿠키 : HP 20 회복 (소지 개수: )");
-                Console.WriteLine($"앨리스의 음료 : MP 10 회복 (소지 개수: )");
-                Console.WriteLine($"사랑의 정수 : HP 100, Mp 50 회복 (소지 개수: )");
+
+                foreach (var portion in DataManager.Instance.PortionDatas)
+                {
+                    Console.WriteLine($" - {portion.ShowPortion(portion)}");
+                }
+
+                //Console.WriteLine($"앨리스의 쿠키 : HP 20 회복 (소지 개수: )");
+                //Console.WriteLine($"앨리스의 음료 : MP 10 회복 (소지 개수: )");
+                //Console.WriteLine($"사랑의 정수 : HP 100, Mp 50 회복 (소지 개수: )");
 
 
             }
