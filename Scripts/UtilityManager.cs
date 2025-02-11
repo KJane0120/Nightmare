c34bc49d650edf9febedc467ab12da8bc6cfdb1e
LeeJiyoung <cjstk7597@gmail.com>
2025-02-12 02:17:48 +0900

    [Feat] 주어진 범위내의 숫자 입력 로직 함수 추가
    잘못된 입력 콘솔 표시 함수 추가
    텍스트 색깔 변경 함수 추가

--- Scripts/UtilityManager.cs
+++ Scripts/UtilityManager.cs
@@ -20,5 +20,52 @@ namespace Nightmare
 
             return e.ToString();
         }
+
+
+        /// <summary>
+        /// 주어진 범위내의 숫자를 입력 로직 함수
+        /// </summary>
+        /// <param name="min">최솟값</param>
+        /// <param name="max">최댓값</param>
+        /// <param name="success">범위내 숫자였을때 불리는 함수</param>
+        /// <param name="failure">범위밖 숫자였을때 불리는 함수</param>
+        static public void InputNumberInRange(int min, int max, Action<int> success, Action failure)
+        {
+            while (true)
+            {
+                Console.Write("\n원하시는 행동을 입력해주세요.\n>>");
+                if (int.TryParse(Console.ReadLine(), out int number) && (min <= number) && (number <= max))
+                {
+                    success?.Invoke(number);
+                }
+                PrintErrorMessage();
+                failure?.Invoke();
+            }
+        }
+
+        static public void PrintErrorMessage()
+        {
+            ColorWriteLine("잘못된 입력입니다!", ConsoleColor.Red);
+        }
+
+        /// <summary>
+        /// 지정 텍스트의 색깔을 변환하는 함수 WriteLine 버전
+        /// </summary>
+        /// <param name="text">색깔 바꿀 텍스트</param>
+        /// <param name="color">색깔</param>
+        static public void ColorWriteLine(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+
+        //Write버전
+        static public void ColorWrite(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(text);
+            Console.ResetColor();
+        }
     }
 }
