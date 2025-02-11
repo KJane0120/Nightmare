b1ce3c26b371151b275a55db8d691ded2b81bb71
KJane0120 <rlawoals5755@gmail.com>
2025-02-12 02:41:08 +0900

    [Feat]회복아이템 사용창의 PrintErrorMsg 구현 및 기타
    UsePortion 호출하는 함수 추가 및 PrintErrorMsg 내용 구현
    Thread.Sleep();을 이용한 콘솔클리어 지연 기능
    추가했습니다.
    +간단한 포션 앞 번호 출력 추가

--- Scripts/Action/Action_Recovery.cs
+++ Scripts/Action/Action_Recovery.cs
@@ -23,16 +23,28 @@ namespace Nightmare
                     { 0,  new Action_Return(0) },
                 };
             }
-
+          
             private void PrintErrorMsg(int number)
             {
-                var selectPortion = DataManager.Instance.PortionDatas[number];
+
+                int index = number - 1;
+                if(index < 0 || index >= DataManager.Instance.PortionDatas.Count)
+                {
+                    Console.WriteLine("잘못된 입력입니다.");
+                    return;
+                }
+                var selectPortion = DataManager.Instance.PortionDatas[index];
                 selectPortion.OnUsePortionEvent = DisPlay;
+
                 selectPortion.UsePortion();
+                
+
             }
             protected override void DisPlay()
             {
+                Thread.Sleep(800);
                 Console.Clear();
+
                 OnInputInvalidActionNumber = PrintErrorMsg;
                 Console.WriteLine("회복");
                 Console.WriteLine("포션을 사용하여 HP나 MP를 회복할 수 있습니다.");
@@ -40,9 +52,11 @@ namespace Nightmare
                 Console.WriteLine("[포션 목록]");
                 Console.WriteLine();
 
+                int i = 0;
                 foreach (var portion in DataManager.Instance.PortionDatas)
                 {
-                    Console.WriteLine($" - {portion.ShowPortion(portion)}");
+                    Console.WriteLine($" - {i + 1}. {portion.ShowPortion()}");
+                    i++;
                 }
 
                 //Console.WriteLine($"앨리스의 쿠키 : HP 20 회복 (소지 개수: )");
