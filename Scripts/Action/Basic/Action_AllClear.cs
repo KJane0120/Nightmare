5f49bf3676fa7c53c41efc6d62de2f51e5974ae0
LeeJiyoung <cjstk7597@gmail.com>
2025-02-13 10:49:40 +0900

    [Feat] 올 클리어 구현

---
+++ Scripts/Action/Basic/Action_AllClear.cs
@@ -0,0 +1,54 @@
+﻿namespace Nightmare
+{
+    public partial class GameManager
+    {
+        public class Action_AllClear : ActionBase
+        {
+            public Action_AllClear(int number) : base(number) { }
+            public override ActionType Type => ActionType.AllClear;
+
+            protected override Dictionary<int, ActionBase> CreateNextActionDic()
+            {
+                return new Dictionary<int, ActionBase>()
+                {
+                    { 0,  new Action_Return(0) },
+                };
+            }
+
+            public override void OnEnter()
+            {
+                SoundManager.PlayBGM("TrueEnding");
+                Console.Clear();
+                DisPlay();
+            }
+
+            protected override void DisPlay()
+            {
+                ASCIIManager.DisplayAlignASCIIArt("TrueEnd", Align.Center, VerticalAlign.Top);
+
+                var clearTexts = new List<string>();
+                clearTexts.Add($"당신은 모든 이야기를 되돌렸습니다.");
+                clearTexts.Add("책을 덮을 시간입니다.");
+                clearTexts.Add($"“옛날 옛적에….”");
+                clearTexts.Add($"1.다시하기");
+                clearTexts.Add($"2.나가기");
+
+                ASCIIManager.AlignText(clearTexts.ToArray(), Align.Center, VerticalAlign.Bottom, 15);
+
+                UtilityManager.InputNumberInRange(1, 2, ReStart, null, "원하시는 행동을 선택해주세요");
+            }
+
+            private void ReStart(int num)
+            {
+                if (num == 1)
+                {
+                    Console.WriteLine("업데이트 예정입니다! 다음 시즌을 기대해주세요 찡긋><");
+                }
+                else
+                {
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
